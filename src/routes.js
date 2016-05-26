eafa791532d59da502efb5e746388957f2acab18
nicolasjhampton <nicolasjhampton@gmail.com>
2016-05-25 18:18:30 -0700

    Non-blocking, asynchronous requests via promises

--- src/routes.js
+++ src/routes.js
@@ -2,44 +2,36 @@
 
 var express = require('express'),
     router = express.Router(),
+    Promise = require('bluebird'),
     twitterRequest = require('./util').twitterRequest;
 
 var root = "https://api.twitter.com/1.1";
-
 var count = "count=5";
-
 var user = "screen_name=nicolasjhampton";
-
 var details = "skip_status=true&include_user_entities=false";
 
-
-var getTimeline = function(req, res, next) {
-  var path = `${root}/statuses/user_timeline.json?${count}&${user}`;
-  twitterRequest(path, res, next);
-};
+var timeline = `${root}/statuses/user_timeline.json?${count}&${user}`;
+var friends = `${root}/friends/list.json?${count}&${user}&${details}`;
+var messages = `${root}/direct_messages.json?${count}&${details}`;
 
 
-var getFriends = function(req, res, next) {
-  var path = `${root}/friends/list.json?${count}&${user}&${details}`;
-  twitterRequest(path, res, next);
-};
-
-
-var getMessages = function(req, res, next) {
-  var path = `${root}/direct_messages.json?${count}&${details}`;
-  twitterRequest(path, res, next);
+var getTwitterInfo = function(req, res, next) {
+  var requests = [twitterRequest(timeline, res, "timeline"),
+                  twitterRequest(friends, res, "friends"),
+                  twitterRequest(messages, res, "messages")];
+  // Non-blocking, asynchronous request
+  Promise.all(requests).then(function() {
+    next();
+  });
 };
 
 
 var renderTwitterAccount = function(req, res, next) {
-  res.locals.timeline = res.locals.json[0];
-  res.locals.friends = res.locals.json[1].users;
-  res.locals.messages = res.locals.json[2];
-
+  res.locals.friends = res.locals.friends.users;
   res.render('index');
 };
 
 
-router.get('/', getTimeline, getFriends, getMessages, renderTwitterAccount);
+router.get('/', getTwitterInfo, renderTwitterAccount);
 
 module.exports = router;
--- src/util.js
+++ src/util.js
@@ -1,26 +1,35 @@
 'use strict';
 
 var OAuth = require('OAuth'),
-    api = require('./apiinfo');
+    api = require('./apiinfo'),
+    Promise = require('bluebird');
+
+
+var consumer_key = api.key;
+var consumer_secret = api.secret;
+var access_token = api.token;
+var access_token_secret = api.tokenSecret;
 
 var oauth = new OAuth.OAuth(
   'https://api.twitter.com/oauth/request_token',
   'https://api.twitter.com/oauth/access_token',
-  api.key,
-  api.secret,
+  consumer_key,
+  consumer_secret,
   '1.0A',
   null,
   'HMAC-SHA1'
 );
 
 
-var twitterRequest = function(path, res, next) {
-  oauth.get(path, api.token, api.tokenSecret, function(err, json, response) {
-    json = JSON.parse(json);
-    res.locals.json = res.locals.json || [];
-    res.locals.json.push(json);
-    console.log(response.statusMessage);
-    next();
+var twitterRequest = function(path, res, name) {
+  return new Promise(function(resolve, reject) {
+    console.log(name); // First call will run "timeline, friends, messages"
+    oauth.get(path, access_token, access_token_secret, function(err, json, response) {
+      if(err) { return reject(err); }
+      res.locals[name] = JSON.parse(json);
+      console.log(name); // Second call will be in asynchronious, random order
+      resolve();
+    });
   });
 };
 
